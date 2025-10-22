e5be87a290ecb8645e51adddfc023a4cc08b7f37
LIPUN ROUT <lipunrout001@gmail.com>
2025-10-23 01:36:10 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const userModel = require("./models/user");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//Read
+app.get("/read", async (req, res) => {
+  let users = await userModel.find();
+  res.render("read", { users });
+});
+
+//Create
+app.post("/create", async (req, res) => {
+  let { name, email, image } = req.body;
+  let createdUser = await userModel.create({
+    name,
+    email,
+    image,
+  });
+  res.redirect("read");
+});
+
+
+//Delete
+app.get('/delete/:id',async(req,res)=>{
+  let deletedUser= await userModel.findOneAndDelete({_id:req.params.id})
+  res.redirect('/read');
+
+})
+
+app.listen(3000, () => {
+  console.log("app was running...");
+});
